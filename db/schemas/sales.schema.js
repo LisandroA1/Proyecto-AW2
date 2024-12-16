f76e3b23f6065ca2d0fd9a36c84fe68e31468cb3
LisandroA1 <Arroyolisandro03@gmail.com>
2024-12-16 00:10:18 -0300

    cambio en schemas de venta

--- db/schemas/sales.schema.js
+++ db/schemas/sales.schema.js
@@ -3,15 +3,17 @@ import mongoose from 'mongoose';
 
 const saleSchema = new mongoose.Schema({
     id_usuario: { type: mongoose.Schema.Types.ObjectId, required: true },
+    nombreUsuario: { type: String, required: true },
     fecha: { type: Date, default: Date.now },
     total: { type: Number, required: true },
     productos: [{
+        nombreProducto: { type: String, required: true }, // Si necesitas el nombre del producto
         productId: { type: mongoose.Schema.Types.ObjectId, required: true }, // Mantén productId para los productos
         cantidad: { type: Number, required: true },
         precioUnitario: { type: Number, required: true }, // Si necesitas el precio unitario
-        nombreProducto: { type: String, required: true }, // Si necesitas el nombre del producto
+        
     }],
-    nombreUsuario: { type: String, required: true },
+    
 });
 
 
